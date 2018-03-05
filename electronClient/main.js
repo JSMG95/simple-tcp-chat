5ef33a09f1f4b11c724c0f90152d30ae66854fa1
JSMG95 <juansalim95@gmail.com>
2018-03-05 12:10:58 -0600

    Add files via upload

---
+++ electronClient/main.js
@@ -0,0 +1,17 @@
+const {app, BrowserWindow} = require('electron')
+const url = require('url');
+const path = require('path');
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({width: 800, height: 600})
+
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+}
+
+app.on('ready', createWindow)
